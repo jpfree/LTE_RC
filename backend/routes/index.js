8f6e8089ece5f1bd9db68e2316a1df033ed5106f
dnjstjr93 <dnjstjr93@gmail.com>
2022-02-04 15:45:56 +0900

    - only use messages with 'ff' header

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,8 +4,8 @@ var router = express.Router();
 
 let rcPort = null;
 let rcPort_info = {
-  Path: 'COM2',
-  BaudRate: 115200
+    Path: 'COM2',
+    BaudRate: 115200
 }
 
 let RCData = '';
@@ -14,71 +14,75 @@ let RCstrFromeGCS = '';
 let RCstrFromeGCSLength = 0;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.sendfile(path.join(__dirname, '../public/index.html'));
+router.get('/', function (req, res, next) {
+    res.sendfile(path.join(__dirname, '../public/index.html'));
 });
 
-router.get('/serialdata', function(req,res,next){
-  res.set({'access-control-allow-origin': '*'});
-  res.send(RCData);
+router.get('/serialdata', function (req, res, next) {
+    res.set({'access-control-allow-origin': '*'});
+    res.send(RCData);
 });
 
 rcPortOpening();
 
 function rcPortOpening() {
-  if (rcPort == null) {
-    rcPort = new SerialPort(rcPort_info.Path, {
-      baudRate: parseInt(rcPort_info.BaudRate, 10),
-    });
+    if (rcPort == null) {
+        rcPort = new SerialPort(rcPort_info.Path, {
+            baudRate: parseInt(rcPort_info.BaudRate, 10),
+        });
 
-    rcPort.on('open', rcPortOpen);
-    rcPort.on('close', rcPortClose);
-    rcPort.on('error', rcPortError);
-    rcPort.on('data', rcPortData);
-  } else {
-    if (rcPort.isOpen) {
-      console.log('This is an already open RC port.')
+        rcPort.on('open', rcPortOpen);
+        rcPort.on('close', rcPortClose);
+        rcPort.on('error', rcPortError);
+        rcPort.on('data', rcPortData);
     } else {
-      rcPort.open();
+        if (rcPort.isOpen) {
+            console.log('This is an already open RC port.')
+        } else {
+            rcPort.open();
+        }
     }
-  }
 }
 
 function rcPortOpen() {
-  console.log('rcPort open. ' + rcPort_info.Path + ' Data rate: ' + rcPort_info.BaudRate);
+    console.log('rcPort open. ' + rcPort_info.Path + ' Data rate: ' + rcPort_info.BaudRate);
 }
 
 function rcPortClose() {
-  console.log('rcPort closed.');
+    console.log('rcPort closed.');
 }
 
 function rcPortError(error) {
-  console.log('[rcPort error]: ' + error.message);
+    console.log('[rcPort error]: ' + error.message);
 
-  setTimeout(rcPortOpening, 2000);
+    setTimeout(rcPortOpening, 2000);
 }
 
 function rcPortData(message) {
-  // RCstrFromeGCS += message.toString('hex').toLowerCase();
-  RCData = message.toString('hex').toLowerCase();
-  // console.log(RCData);
+    RCstrFromeGCS = message.toString('hex').toLowerCase();
+    let header1 = RCstrFromeGCS.substr(0, 2);
+    if (header1 === 'ff') {
+        RCData = RCstrFromeGCS;
+        // console.log(RCData);
+    }
 
-  // while (RCstrFromeGCS.length >= 68) {
-  //   let header1 = RCstrFromeGCS.substr(0, 2);
-  //   if (header1 === 'ff') {
-  //     let RCLength = 34 * 2;
-  //
-  //     // console.log(RCstrFromeGCS.toString('hex'))
-  //     RCData = RCstrFromeGCS.toString('hex');
-  //     // receiveFromRC(RCstrFromeGCS.toString('hex'))
-  //
-  //     RCstrFromeGCS = RCstrFromeGCS.substr(RCLength);
-  //     RCstrFromeGCSLength = 0;
-  //   } else {
-  //     RCstrFromeGCS = RCstrFromeGCS.substr(2);
-  //   }
-  // }
+    // while (RCstrFromeGCS.length >= 68) {
+    //   let header1 = RCstrFromeGCS.substr(0, 2);
+    //   if (header1 === 'ff') {
+    //     let RCLength = 34 * 2;
+    //
+    //     // console.log(RCstrFromeGCS.toString('hex'))
+    //     RCData = RCstrFromeGCS.toString('hex');
+    //     // receiveFromRC(RCstrFromeGCS.toString('hex'))
+    //
+    //     RCstrFromeGCS = RCstrFromeGCS.substr(RCLength);
+    //     RCstrFromeGCSLength = 0;
+    //   } else {
+    //     RCstrFromeGCS = RCstrFromeGCS.substr(2);
+    //   }
+    // }
 }
+
 // setInterval(function () {
 //   console.log(RCData)
 // })
