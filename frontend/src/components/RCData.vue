0bcb20f4963ab511d65656752c97cab149ad0466
dnjstjr93 <dnjstjr93@gmail.com>
2022-02-09 15:46:14 +0900

    - add crc data

--- frontend/src/components/RCData.vue
+++ frontend/src/components/RCData.vue
@@ -358,8 +358,7 @@ import {mixin as VueTimers} from 'vue-timers'
 export default {
     name: 'RCData',
 
-    components: {
-    },
+    components: {},
 
     data() {
         return {
@@ -597,6 +596,24 @@ export default {
             logs: [],
             address: 'ws://localhost:8000',
 
+            crc8_Table: [
+                0, 94, 188, 226, 97, 63, 221, 131, 194, 156, 126, 32, 163, 253, 31, 65,  // 0 ~ 15
+                157, 195, 33, 127, 252, 162, 64, 30, 95, 1, 227, 189, 62, 96, 130, 220,  // 16 ~ 31
+                35, 125, 159, 193, 66, 28, 254, 160, 225, 191, 93, 3, 128, 222, 60, 98,  // 32 ~ 47
+                190, 224, 2, 92, 223, 129, 99, 61, 124, 34, 192, 158, 29, 67, 161, 255,		// 48 ~ 63
+                70, 24, 250, 164, 39, 121, 155, 197, 132, 218, 56, 102, 229, 187, 89, 7,  // 64 ~ 79
+                219, 133, 103, 57, 186, 228, 6, 88, 25, 71, 165, 251, 120, 38, 196, 154,  // 80 ~ 95
+                101, 59, 217, 135, 4, 90, 184, 230, 167, 249, 27, 69, 198, 152, 122, 36,   // 96 ~ 111
+                248, 166, 68, 26, 153, 199, 37, 123, 58, 100, 134, 216, 91, 5, 231, 185,  // 112 ~ 127
+                140, 210, 48, 110, 237, 179, 81, 15, 78, 16, 242, 172, 47, 113, 147, 205,  // 128 ~ 143
+                17, 79, 173, 243, 112, 46, 204, 146, 211, 141, 111, 49, 178, 236, 14, 80,  // 144 ~ 159
+                175, 241, 19, 77, 206, 144, 114, 44, 109, 51, 209, 143, 12, 82, 176, 238,  // 160 ~ 175
+                50, 108, 142, 208, 83, 13, 239, 177, 240, 174, 76, 18, 145, 207, 45, 115,  // 176 ~ 191
+                202, 148, 118, 40, 171, 245, 23, 73, 8, 86, 180, 234, 105, 55, 213, 139, // 192 ~ 207
+                87, 9, 235, 181, 54, 104, 138, 212, 149, 203, 41, 119, 244, 170, 72, 22,  // 208 ~ 223
+                233, 183, 85, 11, 136, 214, 52, 106, 43, 117, 151, 201, 74, 20, 246, 168,  // 224 ~ 239
+                116, 42, 200, 150, 21, 75, 169, 247, 182, 232, 10, 84, 215, 137, 107, 53  // 240 ~ 255
+            ]
         }
     },
     mixins: [VueTimers],
@@ -941,12 +958,27 @@ export default {
             this.ch_value.ch31_value = this.min_max_scaler(this.ch_raw.ch31_raw)
             this.ch_value.ch32_value = this.min_max_scaler(this.ch_raw.ch32_raw)
 
+            let crc = this.Calc_CRC_8(hex_content_each, 33);
+            let hex_crc = crc.toString(16);
+            hex_content_each += hex_crc;
+
             if (this.$store.state.client.connected) {
                 if (!this.radio_cali_flag) {
                     this.$store.state.client.publish('/Mobius/' + this.$store.state.VUE_APP_MOBIUS_GCS + '/RC_Data/' + this.$store.state.VUE_APP_MOBIUS_RC, Buffer.from(hex_content_each, 'hex'))
                     this.RCstrToDrone = ''
                 }
             }
+        },
+        Calc_CRC_8(DataArray, Length) {
+            let i;
+            let crc;
+
+            crc = 0x01;
+            DataArray = Buffer.from(DataArray, 'hex');
+            for (i = 1; i < Length; i++) {
+                crc = this.crc8_Table[crc ^ DataArray[i]];
+            }
+            return crc;
         },
         beforeDestroy() {
             this.destroyConnection()
