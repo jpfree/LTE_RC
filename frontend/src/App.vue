7918851bbdf4e02763790a7f13f1bd1543ed12b3
dnjstjr93 <dnjstjr93@gmail.com>
2022-03-24 01:54:03 +0900

    - 모드, RF 로그 추가

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,12 +4,18 @@
         <v-main>
             <router-view></router-view>
         </v-main>
+        <v-row class="Mode">
+            {{Mode}}
+        </v-row>
+        <v-row v-if="Mode==='[Mode] ' + 'RF'" class="Log">
+            {{UDPLog}}
+        </v-row>
     </v-app>
 </template>
 
 <script>
-
 import RCAppBar from "./components/RCAppBar";
+import EventBus from "@/EventBus";
 
 export default {
     name: 'App',
@@ -18,8 +24,39 @@ export default {
         RCAppBar,
     },
 
-    data: () => ({
-        //
-    }),
+    data() {
+        return {
+            UDPLog: '',
+            Mode:''
+        }
+    },
+    mounted() {
+        EventBus.$on('mode_update', (mode) => {
+            this.Mode = '[Mode] ' + mode
+        })
+        EventBus.$on('log_update', (log) => {
+            this.UDPLog = log
+        })
+    }
 };
 </script>
+
+<style>
+
+.Log {
+    position: absolute;
+    bottom: 4.5%;
+    left: 350px;
+    font-size: 25px;
+    font-weight: bold;
+}
+
+.Mode {
+    position: absolute;
+    bottom: 4%;
+    right: 5%;
+    font-size: 30px;
+    font-weight: bold;
+}
+
+</style>
